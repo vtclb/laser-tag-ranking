11ae45d32c5d6d6c040b8bbe954721670c20387e
vtclb <vartaclub@gmail.com>
2025-04-26 21:48:53 +0300

    Update sortUtils.js

--- scripts/sortUtils.js
+++ scripts/sortUtils.js
@@ -1,6 +1,6 @@
 /** Сортування А→Я */
 export function sortByName(players) {
-  return [...players].sort((a,b)=>a.nick.localeCompare(b.nick,'uk'));
+    return [...players].sort((a,b)=>a.nick.localeCompare(b.nick,'uk'));
 }
 /** Сортування за балами ↓ */
 export function sortByPtsDesc(players) {

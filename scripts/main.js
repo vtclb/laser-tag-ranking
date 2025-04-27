0562c57f024cf00320e6e238a25ca3accc2c6da1
vtclb <vartaclub@gmail.com>
2025-04-27 14:10:14 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,18 @@
-// scripts/main.js
 import { loadPlayers } from './api.js';
 import { initLobby }   from './lobby.js';
 import { initScenario } from './scenario.js';
 
-console.log('main.js завантажено');
-
 const btnLoad   = document.getElementById('btn-load');
 const leagueSel = document.getElementById('league');
 
-btnLoad.addEventListener('click', async () => {
-  console.log('Натиснуто btn-load. Ліга =', leagueSel.value);
+console.log('main.js завантажено');
+btnLoad.onclick = async ()=>{
   try {
     const players = await loadPlayers(leagueSel.value);
-    console.log('Отримані гравці:', players);
     initLobby(players);
     initScenario();
-  } catch (err) {
-    console.error('Помилка loadPlayers:', err);
+  } catch(err) {
+    console.error(err);
     alert('Не вдалося завантажити гравців:\n' + err.message);
   }
-});
+};

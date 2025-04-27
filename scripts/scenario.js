9c2bd6161dca8f274d277d30ba931d33031aaded
vtclb <vartaclub@gmail.com>
2025-04-27 15:03:47 +0300

    Update scenario.js

--- scripts/scenario.js
+++ scripts/scenario.js
@@ -1,49 +1,55 @@
+// scripts/scenario.js
 import { initTeams } from './teams.js';
 import { autoBalance2, autoBalanceN } from './balanceUtils.js';
 import { lobby, setManualCount } from './lobby.js';
 
-const scenarioArea   = document.getElementById('scenario-area');
-const btnAuto        = document.getElementById('btn-auto');
-const btnManual      = document.getElementById('btn-manual');
-const teamSizeSel    = document.getElementById('teamsize');
-const arenaSelect    = document.getElementById('arena-select');
-const arenaCheckboxes= document.getElementById('arena-checkboxes');
-const btnStart       = document.getElementById('btn-start-match');
+const scenarioArea    = document.getElementById('scenario-area');
+const btnAuto         = document.getElementById('btn-auto');
+const btnManual       = document.getElementById('btn-manual');
+const teamSizeSel     = document.getElementById('teamsize');
+const arenaSelect     = document.getElementById('arena-select');
+const arenaCheckboxes = document.getElementById('arena-checkboxes');
+const btnStart        = document.getElementById('btn-start-match');
 
-export function initScenario(){
+export function initScenario() {
   scenarioArea.classList.remove('hidden');
 }
 
-function renderArenaCheckboxes(){
+function renderArenaCheckboxes() {
   arenaCheckboxes.innerHTML = '';
-  document.querySelectorAll('.team-select').forEach(cb=>{
-    const id = cb.dataset.team;
-    arenaCheckboxes.innerHTML +=
-      `<label><input type="checkbox" class="arena-team" data-team="${id}"> Команда ${id}</label>`;
+  Object.keys(window.teams).forEach(k => {
+    const sum = window.teams[k].reduce((s,p)=>s+p.pts,0);
+    arenaCheckboxes.innerHTML += `
+      <label>
+        <input type="checkbox" class="arena-team" data-team="${k}">
+        Команда ${k} (∑ ${sum})
+      </label>`;
   });
 }
 
-function updateStartButton(){
+function updateStartButton() {
   const cnt = document.querySelectorAll('.arena-team:checked').length;
-  btnStart.disabled = cnt!==2;
+  btnStart.disabled = cnt !== 2;
 }
 
-btnAuto.onclick = ()=>{
+// Автобаланс
+btnAuto.onclick = () => {
   const n = +teamSizeSel.value;
   setManualCount(n);
   const data = n===2
-    ? (()=>{const {A,B}=autoBalance2(lobby); return {1:A,2:B}})()
-    : autoBalanceN(lobby,n).reduce((o,a,i)=>(o[i+1]=a,o),{});
-  initTeams(n,data);
+    ? (()=>{ const {A,B}=autoBalance2(lobby); return {1:A,2:B}; })()
+    : autoBalanceN(lobby,n);
+  initTeams(n, data);
   arenaSelect.classList.remove('hidden');
   renderArenaCheckboxes();
   updateStartButton();
 };
 
-btnManual.onclick = ()=>{
+// Ручне
+btnManual.onclick = () => {
   const n = +teamSizeSel.value;
   setManualCount(n);
-  initTeams(n,{});
+  initTeams(n, {});
   arenaSelect.classList.remove('hidden');
   renderArenaCheckboxes();
   updateStartButton();

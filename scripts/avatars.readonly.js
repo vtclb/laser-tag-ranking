ea301cfacf725fe7dae9d5510bc322f17ad6cbf7
vtclb <vartaclub@gmail.com>
2025-09-15 14:37:10 +0300

    fix: update avatar placeholder

--- scripts/avatars.readonly.js
+++ scripts/avatars.readonly.js
@@ -1,5 +1,5 @@
 export const AVATARS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSzum1H-NSUejvB_XMMWaTs04SPz7SQGpKkyFwz4NQjsN8hz2jAFAhl-jtRdYVAXgr36sN4RSoQSpEN/pub?gid=0&single=true&output=csv';
-const AVATAR_PLACEHOLDER = '/assets/avatars/default.png';
+const AVATAR_PLACEHOLDER = '/assets/default_avatars/av0.png';
 
 let mapPromise;
 

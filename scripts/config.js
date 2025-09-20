81116eac7134818eaf17c3b58c1aaf04f7958948
vtclb <vartaclub@gmail.com>
2025-09-21 02:29:53 +0300

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 export const GAS_BASE_URL = 'https://script.google.com/macros/s/AKfycbzhQgbHauvk-ekGVHGRMUnEk-Rt-9M3QI_Jw-bjkRF4jAqpPtXQSDw3BsmivTHdvUY7Gw/exec';
 export const GAS_JSON_URL = GAS_BASE_URL;
-export const AVATARS_BASE = 'https://laser-proxy.vartaclub.workers.dev/avatars/';
+export const AVATARS_BASE = 'https://avatarsproxy.vartaclub.workers.dev';
 export const AVATAR_PLACEHOLDER = 'assets/default_avatars/av0.png';
 export const ASSETS_VER = '2025-09-19-avatars-2';

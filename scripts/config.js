a5ec13db50771189bb08bf0fe0327d848fcaab7b
vtclb <vartaclub@gmail.com>
2025-09-21 12:13:56 +0300

    Update avatar worker endpoints

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,8 @@
 export const GAS_BASE_URL = 'https://script.google.com/macros/s/AKfycbzhQgbHauvk-ekGVHGRMUnEk-Rt-9M3QI_Jw-bjkRF4jAqpPtXQSDw3BsmivTHdvUY7Gw/exec';
 export const GAS_JSON_URL = GAS_BASE_URL;
-export const AVATARS_BASE = 'https://avatarsproxy.vartaclub.workers.dev';
+export const AVATAR_WORKER_BASE = 'https://avatarsproxy.vartaclub.workers.dev';
+export const AVATARS_FEED = 'https://avatarsproxy.vartaclub.workers.dev/feed';
+export const AVATAR_BY_NICK = 'https://avatarsproxy.vartaclub.workers.dev/avatar';
+export const AVATAR_CACHE_BUST = '2025-09-19-avatars-2';
 export const AVATAR_PLACEHOLDER = 'assets/default_avatars/av0.png';
-export const ASSETS_VER = '2025-09-19-avatars-2';
+export const ASSETS_VER = AVATAR_CACHE_BUST;

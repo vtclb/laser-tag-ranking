151abb8192d7239b75aa581632a911345d7ee1c2
vtclb <vartaclub@gmail.com>
2025-12-10 00:30:09 +0200

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,12 @@
 export const LEAGUE = location.pathname.endsWith('sunday.html') ? 'sundaygames' : 'kids';
+export const LEAGUE = (() => {
+  const path = location.pathname.toLowerCase();
+
+  if (path.includes('sunday')) return 'sundaygames';
+  if (path.includes('old')) return 'olds';
+  if (path.includes('kids')) return 'kids';
+  if (path.includes('index')) return 'kids';
+
+  return 'kids';
+})();
+
